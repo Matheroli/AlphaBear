eb1d1e488186076d0d497c8898479f7007a9958e
Matheroli <68776425+Matheroli@users.noreply.github.com>
2021-01-27 11:14:44 +0100

    Update index.php

--- index.php
+++ index.php
@@ -14,13 +14,22 @@ Certaines pages ont été réalisées grâce aux templates de https://freefronte
     <meta property="og:site_name" content="Site Web de Matheroli">
     <meta property="og:url" content="https://matheroli-mcland.000webhostapp.com">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Matheroli | Accueil</title>
+    <title>AlphaBear | Accueil</title>
     <meta charset="utf-8">
-    <link rel="shortcut icon" type="image/png" href="assets/bear.png"/>
+    <link rel="shortcut icon" type="image/png" href="assets/bear1.png"/>
 </head>
-<h1><u>Bienvenue sur mon site !!</u></h1>
-<p class="paragraphe">Pour afficher le code source faites Ctrl+U</p>
 <body>
+    <h1><u>Bienvenue sur mon site !!</u></h1>
+    <p class="paragraphe">Pour afficher le code source faites Ctrl+U</p>
+    <div>
+        <div class="links">
+        <div class="link">
+            <a href="https://discord.gg/8HUqgnw" target="_BLANK"><img width="75" height="75" src="assets/discord.png"></a>
+        </div>
+        <div class="link">
+            <a href="https://github.com/Matheroli/SiteWeb#readme" target="_BLANK"><img width="75" height="75" src="assets/github.png"></a>
+        </div>
+    </div>
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-1616K2DP9H"></script>
     <script>
@@ -36,7 +45,8 @@ Certaines pages ont été réalisées grâce aux templates de https://freefronte
     <iframe src="https://fr.euronews.com/embed/timeline" scrolling="no" style="border:none; min-height:425px; width:100%; height:100%;"></iframe>
 <style>
     h1, .paragraphe{color: rgb(0, 255, 213);text-align: center;}
-    body{background: linear-gradient(to right, red,orange);height: 100%;}
+    body{background: linear-gradient(to right, purple,green);height: 100%;}
+    .link{display: inline-block;}
 </style>
 </body>
 </html>
