f8b3e3650962480814f6ad157298787adc78ad5b
Matheroli <68776425+Matheroli@users.noreply.github.com>
2021-03-13 17:21:46 +0100

    Create header.php

---
+++ config/header.php
@@ -0,0 +1,17 @@
+<head>
+    <meta property="og:type" content="article">
+    <meta property="og:image" content="https://matheroli-mcland.000webhostapp.com/assets/bear.png">
+    <meta property="og:title" content="Site Web de Matheroli">
+    <meta property="og:description" content="J'avais envie de créer un site web alors j'en ai fait un.">
+    <meta property="og:site_name" content="Site Web de Matheroli">
+    <meta property="og:url" content="https://matheroli-mcland.000webhostapp.com">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Website</title>
+    <meta charset="utf-8">
+    <link rel="shortcut icon" type="image/png" href="assets/bear1.png"/>
+    <link rel="stylesheet" type="text/css" href="styles/scrollbar-navbar.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
